c148942de766a5a0ef486913a501ce1828e2187f
Michael Wynholds <mike@carbonfive.com>
2011-11-02 15:21:00 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'thin'
+gem 'pg'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'heroku'
+  gem 'taps'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourbot::Application.config.secret_token = '850336ffb1dbccafb2b466ea5bdf07d55b19bf6c767134585a5ddf05a6466a387d1d27e4097c591880d3f5282ff08ccb1c5d1ba8f2bf674ca63c53562fa12c0f'
