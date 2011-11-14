8ee5f7aaffad80ed64aefa26bce09c1f97fcb6f8
Michael Wynholds <mike@carbonfive.com>
2011-11-14 15:53:11 -0800

    Better tourbot JS pipeline

--- app/assets/javascripts/tourbot.js
+++ app/assets/javascripts/tourbot.js
@@ -1,15 +1,3 @@
-//= require_self
-
-var _tourconfig = {
-  source: 'tourbot',
-  interactions:
-          [
-            { name: '1', inbound: '#first-name', outbound: '#last-name', type: 'text', message: 'Tell me your name' },
-            { name: '2', inbound: 'input.gender', offset: { x: 100, y: 0 }, type: 'clickable', message: 'Tell me whether you\'re a boy or girl' },
-            { name: '3', inbound: 'fieldset.submit input', type: 'clickable', message: 'Now click this button!' }
-          ]
-};
-
 (function() {
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
   var root = this;
