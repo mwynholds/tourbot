1aba9a2403dac2d501315d03ba8102d682f78184
Michael Wynholds <mike@carbonfive.com>
2011-11-20 08:43:53 -0800

    Proxying is working for cdff

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Tourbot::Application.routes.draw do
   resources :interactions, :only => [:create, :show]
   resources :demos, :only => [:show]
   resources :diagnostics, :only => [:show]
+
+  match '/proxy/:site(/*path)' => 'proxies#proxy', :via => [:get, :post], :constraints => { :path => /.*/ }
 end
