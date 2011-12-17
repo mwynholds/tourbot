f432c3614c80e3456c3df1af92d9ae4b71db9708
Michael Wynholds <mike@carbonfive.com>
2011-12-16 17:07:33 -0800

    Simpler tourbot loading

--- app/assets/javascripts/tourbot.js
+++ app/assets/javascripts/tourbot.js
@@ -6,51 +6,20 @@
 
 (function(root) {
 
-  var load_tourbot = function() {
-    var detect = root.tourbot.BrowserDetect;
-
-    var PIE = base_url() + '/assets/PIE-1.0beta5.js';
-    var pie = function() { return detect.browser != 'Explorer' || typeof window['PIE'] != 'undefined'; };
-    wait_for(pie, PIE, ready_to_go);
-  };
-
-  var tb_jquery = root.jQuery;
+  var $ = root.jQuery;
   root.jQuery = null;
   root.$ = null;
-  load_tourbot();
 
-  function wait_for(is_ready, url, callback) {
-    if (! is_ready()) {
-      var head = document.getElementsByTagName('head')[0] || document.documentElement;
-      var script = document.createElement('script');
-
-      script.src = url;
-      script.type = 'text/javascript';
-      script.async = true;
-
-      head.insertBefore(script, head.firstChild);
-    }
-
-    var wait = function() {
-      if (! is_ready()) {
-        window.setTimeout(wait, 100);
-      }
-      else {
-        callback();
-      }
-    };
-
-    wait();
+  var detect = root.tourbot.BrowserDetect;
+  if (detect.browser == 'Explorer') {
+    $.getScript( base_url() + '/assets/PIE-1.0beta5.js' );
   }
 
-  function ready_to_go() {
-    // super hack to save Array.push()
-    Array.prototype.tb_push = Array.prototype.push;
-    tb_jquery(document).ready( function() {
-      Array.prototype.push = Array.prototype.tb_push;
-      new root.tourbot.Tourbot(tb_jquery, base_url());
-    });
-  }
+  Array.prototype.tb_push = Array.prototype.push;
+  $(document).ready( function() {
+    Array.prototype.push = Array.prototype.tb_push;
+    new root.tourbot.Tourbot($, base_url());
+  });
 
   function base_url() {
     var loc = root.location;
